82572137823b0634d1c273e7b145b6f1c7a78315
jijinkh <jijinkharidas10@gmail.com>
2020-05-20 16:25:26 +0530

    fixed backend

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var app = express()
 var path = require('path')
+var dbScripts = require('./src/js/Database.js')
+var bodyParser = require('body-parser');
 
 var css = path.join(__dirname, 'components/css')
 var img = path.join(__dirname, 'components/img')
@@ -9,32 +11,64 @@ var assets = path.join(__dirname, 'assets')
 
 
 // Loads the static file into the app
-app.use('/',express.static(css))
-app.use('/',express.static(img))
-app.use('/',express.static(assets))
+app.use('/', express.static(css))
+app.use('/', express.static(img))
+app.use('/', express.static(assets))
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
-
-app.get('/', (req, res)=>{
-    res.sendFile(path.join(templates + '/index.html'))
+app.get('/', (req, res) => {
+	res.sendFile(path.join(templates + '/index.html'))
 })
 
-app.get('/index.html', (req, res)=>{
-    res.sendFile(path.join(templates + '/index.html'))
+app.get('/index.html', (req, res) => {
+	res.sendFile(path.join(templates + '/index.html'))
 })
 
-app.get('/newtask.html', (req, res)=>{
-    res.sendFile(path.join(templates + '/newtask.html'))
+app.get('/newtask.html', (req, res) => {
+	res.sendFile(path.join(templates + '/newtask.html'))
 })
 
-app.get('/about.html', (req, res)=>{
-    res.sendFile(path.join(templates + '/about.html'))
+app.post('/newtask.html', (req, res) => {
+	var title = req.body.tasktitle
+	var description = req.body.description
+	var date = req.body.date
+	dbScripts.insert(title, description, false)
+	res.sendFile(path.join(templates + '/newtask.html'))
 })
 
-app.get('/currenttask.html', (req, res)=>{
-    res.sendFile(path.join(templates + '/currenttask.html'))
+app.get('/about.html', (req, res) => {
+	res.sendFile(path.join(templates + '/about.html'))
+})
+
+app.get('/currenttask.html', (req, res) => {
+	var MongoClient = require('mongodb').MongoClient
+	var TaskList = []
+	var url = 'mongodb://localhost:27017'
+	MongoClient.connect(url, function (err, db) {
+		var dbo = db.db('Taskmanager')
+		var cursor = dbo.collection('tasks').find()
+		if (err) throw err
+		cursor.forEach(function (task, err) {
+			if (err) throw err
+			TaskList.push(task)
+			console.log(task)
+		}, function () {
+			db.close()
+			res.render(path.join(templates + '/currenttask'), { tasks: TaskList })
+		})
+	})
+})
+
+app.post('/currenttask.html', (req, res) => {
+	id = req.query.q
+	dbScripts.delete(id)
+	console.log(id)
+	res.redirect('/currenttask.html')
 })
 
 
 app.listen(3000)
+
 console.log("Listening in port 3000")
---
+++ src/js/Database.js
@@ -0,0 +1,59 @@
+exports.createDB = () => {
+    var client = require('mongodb').MongoClient
+    var url = 'mongodb://localhost:27017/Taskmanager'
+
+    client.connect(url, (err, db) => {
+        if (err) throw err
+        console.log('Database created')
+        db.close()
+    })
+}
+
+exports.collection = () => {
+    var client = require('mongodb').MongoClient
+    var url = 'mongodb://localhost:27017/'
+    client.connect(url, (err, db) => {
+        if (err){
+            createDB()
+        }
+        var dbo = db.db('Taskmanager')
+        dbo.createCollection("tasks", (err, res) => {
+            if(err) throw err
+            console.log("Collection created")
+        })
+    })
+}
+
+exports.insert = (tId, tDesc, tCompletion) => {
+    var client = require('mongodb').MongoClient
+    var url = 'mongodb://localhost:27017/'
+    var obj = { Id : tId, Desc : tDesc, Completion : tCompletion }
+    client.connect(url, (err, db) => {
+        if(err){
+            throw err
+        }
+        var dbo = db.db('Taskmanager')
+        dbo.collection('tasks').insertOne(obj, (err, res) => {
+            if (err) throw err
+            console.log('inserted data')
+            db.close()
+        })
+    })
+}
+
+
+exports.delete = (id) => {
+    var ObjectId = require('mongodb').ObjectID;
+    var client = require('mongodb').MongoClient
+    var url = 'mongodb://localhost:27017'
+    client.connect(url, (err, db) => {
+        if(err) throw err
+        var dbo = db.db('Taskmanager')
+        query = {_id : new ObjectId(id)}
+        dbo.collection('tasks').deleteOne(query, function(err, obj){
+            if(err) throw err
+            console.log("deleted record")
+            db.close()
+        })
+    })
+}
